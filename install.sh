2d2fed439f387f3f21d3992dc5089caca149307a
Daniel Moran <danielheth@hotmail.com>
2014-07-21 19:35:38 -0500

    added installer script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Installing ddclient for cloudflare"
+
+
+echo "Copying files..."
+cp etc_rc.d_init.d_ddclient.ubuntu /etc/init.d/ddclient
+cp ddclient /usr/sbin/
+mkdir /etc/ddclient
+cp ddclient.conf /etc/ddclient/ddclient.conf
+echo "Done"
+
+echo "\nTODO:  Now you need to edit the /etc/ddclient/ddclient.conf and add your cloudflare username/api_key, zone and fqdn for this client.\n"
