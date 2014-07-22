739a559cab9dd2b3103ea651efa622c7f23a2c45
Daniel Moran <danielheth@hotmail.com>
2014-07-21 19:47:42 -0500

    updated installer with dependancies

--- install.sh
+++ install.sh
@@ -7,9 +7,11 @@ apt-get install perl libjson-any-perl libio-socket-ssl-perl -y
 
 echo "Copying files..."
 cp etc_rc.d_init.d_ddclient.ubuntu /etc/init.d/ddclient
+chmod +x /etc/init.d/ddclient
 cp ddclient /usr/sbin/
 mkdir /etc/ddclient
 cp ddclient.conf /etc/ddclient/ddclient.conf
 echo "Done"
 
 echo "\nTODO:  Now you need to edit the /etc/ddclient/ddclient.conf and add your cloudflare username/api_key, zone and fqdn for this client.\n"
+echo "\nWhen you are finished editing, remember to run sudo service ddclient start"
