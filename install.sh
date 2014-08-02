82b07c8cec4180b16c483ff10b501a37d85ac117
danielheth <danielheth@hotmail.com>
2014-08-02 14:16:20 -0500

    updating installer

--- install.sh
+++ install.sh
@@ -1,17 +1,42 @@
 #!/bin/sh
 
+
 echo "Installing ddclient for cloudflare"
+
+read -p "What zone is this system a part of (domain name listed on cloudflare)? " zone
+
+read -p "What is your cloudflare username? " login
+
+read -p "What is your cloudflare API key (look under account settings)? " password
+
+read -p "What is the fully qualified (hostname.$zone) for this device? " fqdn
+
+
+
+
 
 echo "Installing Pre-reqs"
 apt-get install perl libjson-any-perl libio-socket-ssl-perl -y
 
-echo "Copying files..."
+echo "Installing ddclient"
 cp etc_rc.d_init.d_ddclient.ubuntu /etc/init.d/ddclient
 chmod +x /etc/init.d/ddclient
 cp ddclient /usr/sbin/
+mkdir /var/cache/ddclient
+
+echo "Configuring ddclient"
 mkdir /etc/ddclient
 cp ddclient.conf /etc/ddclient/ddclient.conf
-echo "Done"
+echo "zone=$zone," >> /etc/ddclient/ddclient.conf
+echo "login=$login," >> /etc/ddclient/ddclient.conf
+echo "password=$password" >> /etc/ddclient/ddclient.conf
+echo "$fqdn," >> /etc/ddclient/ddclient.conf
 
-echo "\nTODO:  Now you need to edit the /etc/ddclient/ddclient.conf and add your cloudflare username/api_key, zone and fqdn for this client.\n"
-echo "\nWhen you are finished editing, remember to run sudo service ddclient start"
+
+echo "Adding to Startup"
+update-rc.d ddclient defaults
+
+echo "Starting ddclient"
+service ddclient Starting
+
+echo "TODO:  Log into your CloudFlare account and validate the public ip address has been configured."
